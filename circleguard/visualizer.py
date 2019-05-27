8bee8090d6d16b777a8b4541a6abfea5bde60bbb
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-05-27 19:33:16 +0200

    attempt to render replay without interpolation

--- circleguard/visualizer.py
+++ circleguard/visualizer.py
@@ -1,8 +1,4 @@
-
-from circleguard import Replay
-from circleguard import utils
-
-import threading
+from circleguard import enums
 # pylint: disable=no-name-in-module
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QMainWindow, QGridLayout, QSlider, QPushButton, QStyle
@@ -16,8 +12,11 @@ class _Renderer(QWidget):
     def __init__(self, replay1, replay2, parent=None):
         super(_Renderer, self).__init__(parent)
         # initialize variables
-        self.counter = 0
-        self.old_multiplier = 1
+        self.current = 0
+        self.frame_change = 0
+        self.counter1 = 0
+        self.counter2 = 0
+        self.chosen_replay = -1
         self.buffer1 = []
         self.buffer2 = []
         self.paused = False
@@ -27,17 +26,31 @@ class _Renderer(QWidget):
         # interpolate replays
         self.data1 = replay1.as_list_with_timestamps()
         self.data2 = replay2.as_list_with_timestamps()
-        self.data1, self.data2 = utils.interpolate(self.data1, self.data2, unflip=False)
-        self.data1 = utils.resample(self.data1, 60)
-        self.data2 = utils.resample(self.data2, 60)
-        self.data1 = [(512 - d[1], 384 - d[2]) for d in self.data1]
-        self.data2 = [(512 - d[1], 384 - d[2]) for d in self.data2]
+        # flip replay if one is with hr
+        bit_values_gen1 = self.bits(replay1.mods)
+        bit_values_gen2 = self.bits(replay2.mods)
+        self.enabled_mods_replay_1 = frozenset(enums.Mod(mod_val) for mod_val in bit_values_gen1)
+        self.enabled_mods_replay_2 = frozenset(enums.Mod(mod_val) for mod_val in bit_values_gen2)
+        flip1 = enums.Mod.HardRock.value in [mod.value for mod in self.enabled_mods_replay_1]
+        flip2 = enums.Mod.HardRock.value in [mod.value for mod in self.enabled_mods_replay_2]
+        if flip1 ^ flip2:
+            self.data1 = [(d[0], 512 - d[1], d[2]) for d in self.data1]
+        else:
+            self.data1 = [(d[0], 512 - d[1], 384 - d[2]) for d in self.data1]
+        self.data2 = [(d[0], 512 - d[1], 384 - d[2]) for d in self.data2]
         self.replay_len = len(self.data1)
-        print(f"replay is {self.replay_len/60} seconds long")
-
         self.timer.timeout.connect(self.next_frame)
         self.timer.start(1000/60)  # next frame every 1/60sec
         self.next_frame()
+
+    @staticmethod
+    def bits(n):
+        if n == 0:
+            yield 0
+        while n:
+            b = n & (~n + 1)
+            yield b
+            n ^= b
 
     def paintEvent(self, event):  # finished
         """
@@ -55,40 +68,68 @@ class _Renderer(QWidget):
         alpha_step = 255/len(self.buffer2)
         for i in range(len(self.buffer1)-1):
             painter.setPen(QPen(QColor(63, 127, 255, (i*alpha_step)), 1))
-            painter.drawLine(self.buffer1[i][0], self.buffer1[i][1], self.buffer1[i+1][0], self.buffer1[i+1][1])
+            painter.drawLine(self.buffer1[i][1], self.buffer1[i][2], self.buffer1[i+1][1], self.buffer1[i+1][2])
         for i in range(len(self.buffer2)-1):
             painter.setPen(QPen(QColor(255, 127, 63, (i*alpha_step)), 1))
-            painter.drawLine(self.buffer2[i][0], self.buffer2[i][1], self.buffer2[i+1][0], self.buffer2[i+1][1])
+            painter.drawLine(self.buffer2[i][1], self.buffer2[i][2], self.buffer2[i+1][1], self.buffer2[i+1][2])
 
-        # todo this is next section is wrong, since we should draw the raw events and not the interpolated events
         for i in range(len(self.buffer1)):
             painter.setPen(QPen(QColor(255, 127, 63, (i*alpha_step)), 3))
-            painter.drawPoint(int(self.buffer1[i][0]), int(self.buffer1[i][1]))
+            painter.drawPoint(int(self.buffer1[i][1]), int(self.buffer1[i][2]))
 
         for i in range(len(self.buffer2)):
             painter.setPen(QPen(QColor(63, 127, 255, (i*alpha_step)), 3))
-            painter.drawPoint(int(self.buffer2[i][0]), int(self.buffer2[i][1]))
+            painter.drawPoint(int(self.buffer2[i][1]), int(self.buffer2[i][2]))
+        painter.setPen(QPen(QColor(128, 128, 128), 1))
+        painter.drawText(0, 25, f"frame: {self.current} | frame change : {str(self.frame_change).rjust(3,'0')} | step was at replay{self.chosen_replay}")
 
-    def next_frame(self):  # finished
+    def next_frame(self):
         if self.paused:
             return
-        if self.counter >= self.replay_len-1:
-            self.reset()
-        self.buffer1 = self.data1[self.counter:self.counter+(int(15*self.old_multiplier))]
-        self.buffer2 = self.data2[self.counter:self.counter+(int(15*self.old_multiplier))]
+        self.current += int(1000/120)  # this is completely wrong but it kinda works, sooooo
+        next_frame_1 = self.data1[self.counter1 + 1][0]
+        next_frame_2 = self.data2[self.counter2 + 1][0]
+
+        # plan is to skip frames in the past
+        while next_frame_1 < self.current or next_frame_2 < self.current:
+            self.counter1 += 1
+            self.counter2 += 1
+            next_frame_1 = self.data1[self.counter1+1][0]
+            next_frame_2 = self.data2[self.counter2+1][0]
+
+        if next_frame_1 == next_frame_2:
+            self.chosen_replay = "1&2"
+            self.frame_change = 0
+            self.current = next_frame_1
+            self.counter1 += 1
+            self.counter2 += 1
+        elif next_frame_1 < next_frame_2:
+            self.chosen_replay = 1
+            self.frame_change = next_frame_1-self.current
+            self.current = next_frame_1
+            self.counter1 += 1
+        else:
+            self.chosen_replay = 2
+            self.frame_change = next_frame_2-self.current
+            self.current = next_frame_2
+            self.counter2 += 1
+        self.buffer1 = self.data1[self.counter1:self.counter1+(int(15))]
+        self.buffer2 = self.data2[self.counter2:self.counter2+(int(15))]
         self.update()
-        self.update_signal.emit(self.counter)
-        self.counter += 1
 
     def reset(self):
-        self.counter = 0
+        self.counter1 = 0
+        self.counter2 = 0
+        self.current = 0
 
-    def seek_to(self, kek):
-        self.counter = kek
+    def seek_to(self, position):
+        return position
+        """
         if self.paused:
             self.paused = False
             self.next_frame()
             self.paused = True
+        """
 
     def pause(self):
         if self.paused:
