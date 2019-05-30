92e83cd9e7050c40870c98b0a702312941c569aa
tybug <orionldevoe@gmail.com>
2019-05-30 12:39:44 -0400

    properly visualize both frames in real time

--- circleguard/visualizer.py
+++ circleguard/visualizer.py
@@ -10,7 +10,8 @@ from PyQt5.QtGui import QColor, QPainterPath, QPainter, QPen
 
 WIDTH_LINE = 1
 WIDTH_POINT = 3
-
+FRAMES_ON_SCREEN = 15 # how many frames for each replay to draw on screen at a time
+                      # (though some will have high alpha values and be semi transparent)
 PEN_BLUE = QPen(QColor(63, 127, 255))
 PEN_RED = QPen(QColor(255, 127, 63))
 
@@ -20,20 +21,18 @@ class _Renderer(QWidget):
     def __init__(self, replay1, replay2, parent=None):
         super(_Renderer, self).__init__(parent)
         # initialize variables
-        self.current = 0
-        self.frame_change = 0
-        self.counter1 = 0
-        self.counter2 = 0
-        self.chosen_replay = -1
+        self.current_time = 0
+        self.pos1 = -1 # so our first frame is at data[0] since we do pos + 1
+        self.pos2 = -1
         self.buffer1 = []
         self.buffer2 = []
+        self.buffer_additions1 = []
+        self.buffer_additions2 = []
         self.paused = False
-        self.path1 = QPainterPath()
-        self.path2 = QPainterPath()
-        self.timer = QTimer(self)
-        # interpolate replays
-        self.data1 = replay1.as_list_with_timestamps()
-        self.data2 = replay2.as_list_with_timestamps()
+
+        self.data1 = replay1.as_list_with_timestamps() #t,x,y
+        self.data2 = replay2.as_list_with_timestamps() #t,x,y
+
         # flip replay if one is with hr
         mods1 = [Mod(mod_val) for mod_val in utils.bits(replay1.mods)]
         mods2 = [Mod(mod_val) for mod_val in utils.bits(replay2.mods)]
@@ -41,28 +40,96 @@ class _Renderer(QWidget):
         flip2 = Mod.HardRock in mods2
         if(flip1 ^ flip2): # xor, if one has hr but not the other
             for d in self.data1:
-                d[1] = 384 - d[1]
+                d[1] = 384 - d[1] # flip the x's
 
-        self.replay_len = len(self.data1)
+        self.replay_len = max(len(self.data1), len(self.data2))
+        self.next_frame()
+
+        self.timer = QTimer(self)
         self.timer.timeout.connect(self.next_frame_from_timer)
         self.timer.start(1000/60)  # 60fps (1000ms/60frames)
+
+
+    def next_frame_from_timer(self):
+        """
+        Has the same effect as next_frame except if paused, where it returns. This is to allow
+        the back/forward buttons to advance frame by frame while still paused (as they connect directly to next
+        and previous frame), while still pausing the automatic timer advancement.
+        """
+        if self.paused:
+            return
         self.next_frame()
+
+    def next_frame(self):
+        """
+        Adds frames to two buffer_additions variables. Every time this method is called both arrays are guaranteed
+        to have at least one value added (after being cleared by paintEvent), but one may have more if there are frames
+        between it and the larger time. See comments in this method for a more helpful walkthrough (hopefully)
+
+        buffer_additions will be drawn by paintEvent in addition to the buffers, where buffer_additions are the latest
+        replays and the buffers will have the oldest frames removed from them in equal amounts to the additions so that
+        each buffer has at most FRAMES_ON_SCREEN (could have less if it's the start or the end; don't remove from buffer
+        if it has less than FRAMES_ON_SCREEN)
+        """
+
+        #documentation assumes data arrays have time vals of
+        # [3, 4, 6, 9] data1
+        # [1, 2, 3.5, 7] data2
+        next_frame1 = self.data1[self.pos1 + 1]
+        next_frame2 = self.data2[self.pos2 + 1]
+        next_time1 = next_frame1[0] # 3
+        next_time2 = next_frame2[0] # 1
+        self.buffer_additions1.append(next_frame1)
+        self.buffer_additions2.append(next_frame2)
+        self.pos1 += 1
+        self.pos2 += 1
+
+        # if this enters, replay1 is the farther along
+        while(next_time1 > next_time2): # 3 > 1
+            self.pos2 += 1
+            frame = self.data2[self.pos2]
+            next_next_time2 = frame[0] # 2
+            if(next_next_time2 > next_time1): # 2 ≯ 3 so don't enter
+                break # only want to draw frames inbetweem the smaller and larger time, don't go over the larger time
+            self.buffer_additions2.append(frame)
+            next_time2 = next_next_time2 # next_time2 is now 2, loops again but next_next_time2
+                                         # becomes 3.5 which is greater than next_time1 so we break
+
+         # if first loop is entered (which it is in documentation) then this never should be
+         # because of the break
+         # if this enters, replay2 is farther along
+        while(next_time2 > next_time1):
+            self.pos1 += 1
+            frame = self.data1[self.pos1]
+            next_next_time1 = frame[0]
+            if(next_next_time1 > next_time2):
+                break
+            self.buffer_additions1.append(frame)
+            next_time1 = next_next_time1
+
+        # if neither enter, both have the exact same next frame, so just draw both and call it a day
+
+        self.update_signal.emit(1)
+        self.update()
 
 
     def paintEvent(self, event):  # finished
         """
-        painter3 = QPainter(self)
-        image = QImage("C:/Users/Master/Documents/GitHub/circleguard-script/circleguard/resources/logo.png")
-        painter3.drawImage(self.width() / 2 - 100, self.height() / 2 - 100, image)  # sprite example
-        image = image.scaled(20, 20, Qt.KeepAspectRatio)  # sprite resize example
-        painter3.drawImage(0, 0, image)  # sprite example
-        painter3.drawImage(self.width() - 20, self.height() - 20, image)  # sprite example
-        painter3.drawImage(0, self.height() - 20, image)  # sprite example
-        painter3.drawImage(self.width() - 20, 0, image)  # sprite example
+        Called whenever self.update() is called
         """
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
-        alpha_step = 255/len(self.buffer2)
+        self.buffer1 += self.buffer_additions1
+        self.buffer2 += self.buffer_additions2
+
+        # if less than FRAMES_ON_SCREEN, becomes 0. See https://math.stackexchange.com/a/3018840
+        extra1 = abs(len(self.buffer1) - FRAMES_ON_SCREEN) + (len(self.buffer1) - FRAMES_ON_SCREEN)
+        extra2 = abs(len(self.buffer2) - FRAMES_ON_SCREEN) + (len(self.buffer2) - FRAMES_ON_SCREEN)
+
+        del self.buffer1[:extra1] # delete oldest extra frames
+        del self.buffer2[:extra2]
+
+        alpha_step = 255/FRAMES_ON_SCREEN
         for i in range(len(self.buffer1)-1):
             p1 = QPoint(self.buffer1[i][1], self.buffer1[i][2])
             p2 = QPoint(self.buffer1[i+1][1], self.buffer1[i+1][2])
@@ -82,7 +149,9 @@ class _Renderer(QWidget):
             self.draw_point(painter, PEN_RED, i*alpha_step, p)
 
         painter.setPen(QPen(QColor(128, 128, 128), 1))
-        painter.drawText(0, 25, f"frame: {self.current} | frame change : {str(self.frame_change).rjust(3,'0')} | step was at replay{self.chosen_replay}")
+        painter.drawText(0, 25, f"pos1: {self.pos1} | pos2: {self.pos2}")
+        self.buffer_additions1.clear()
+        self.buffer_additions2.clear()
 
     def draw_line(self, painter, pen, alpha, start, end):
         """
@@ -96,9 +165,15 @@ class _Renderer(QWidget):
             QPoint start: The start of the line.
             QPoint end: The end of the line.
         """
-        pen.setWidth(WIDTH_LINE)
-        pen.color().setAlpha(alpha)
-        painter.setPen(pen)
+
+
+        # I had pen.color().setAlpha(alpha), but it wouldn't actually change the alpha.
+        # doing pen.setColor(pen.color()) after that didn't work either so I've resorted to
+        # a possibly inefficient method by recreating the pen
+        c = pen.color()
+        pen_ = QPen(QColor(c.red(), c.green(), c.blue(), alpha))
+        pen_.setWidth(WIDTH_LINE)
+        painter.setPen(pen_)
         painter.drawLine(start, end)
 
     def draw_point(self, painter, pen, alpha, point):
@@ -111,54 +186,13 @@ class _Renderer(QWidget):
             Integer alpha: The alpha level from 0-255 to set the point to.
             QPoint point: The QPoint representing the coordinates at which to draw the point.
         """
-        pen.setWidth(WIDTH_POINT)
-        pen.color().setAlpha(alpha)
-        painter.setPen(pen)
+        c = pen.color()
+        pen_ = QPen(QColor(c.red(), c.green(), c.blue(), alpha))
+        pen_.setWidth(WIDTH_POINT)
+        painter.setPen(pen_)
         painter.drawPoint(point)
 
 
-    def next_frame_from_timer(self):
-        """
-        Has the same effect as next_frame except if paused, where it returns. This is to allow
-        the back/forward buttons to advance frame by frame while still paused (as they connect directly to next
-        and previous frame), while still pausing the automatic timer advancement.
-        """
-        if self.paused:
-            return
-        self.next_frame()
-
-    def next_frame(self):
-        self.current += int(1000/120)  # this is completely wrong but it kinda works, sooooo
-        next_frame_1 = self.data1[self.counter1 + 1][0]
-        next_frame_2 = self.data2[self.counter2 + 1][0]
-
-        # plan is to skip frames in the past
-        while next_frame_1 < self.current or next_frame_2 < self.current:
-            self.counter1 += 1
-            self.counter2 += 1
-            next_frame_1 = self.data1[self.counter1+1][0]
-            next_frame_2 = self.data2[self.counter2+1][0]
-
-        if next_frame_1 == next_frame_2:
-            self.chosen_replay = "1&2"
-            self.frame_change = 0
-            self.current = next_frame_1
-            self.counter1 += 1
-            self.counter2 += 1
-        elif next_frame_1 < next_frame_2:
-            self.chosen_replay = 1
-            self.frame_change = next_frame_1-self.current
-            self.current = next_frame_1
-            self.counter1 += 1
-        else:
-            self.chosen_replay = 2
-            self.frame_change = next_frame_2-self.current
-            self.current = next_frame_2
-            self.counter2 += 1
-        self.buffer1 = self.data1[self.counter1:self.counter1 + 15]
-        self.buffer2 = self.data2[self.counter2:self.counter2 + 15]
-        self.update_signal.emit(1)
-        self.update()
 
     def reset(self):
         self.counter1 = 0
