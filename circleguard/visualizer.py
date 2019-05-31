c2b487f0bf703aa1eb0a05e9fc2acb4873f73459
tybug <orionldevoe@gmail.com>
2019-05-31 09:13:38 -0400

    no outline on progress slider focus

--- circleguard/visualizer.py
+++ circleguard/visualizer.py
@@ -243,6 +243,7 @@ class _Interface(QWidget):
 
         self.slider.setRange(0, self.renderer.replay_len)
         self.slider.setValue(0)
+        self.slider.setStyleSheet("outline: none;")
         self.renderer.update_signal.connect(self.update_slider)
 
         self.layout.addWidget(self.renderer, 0, 0, 1, 10)
