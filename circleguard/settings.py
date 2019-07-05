c339b9b522dafd66b21fd81bf1e7e1624305c949
tybug <orionldevoe@gmail.com>
2019-07-05 13:02:45 -0400

    shorter timestamp syntax

--- circleguard/settings.py
+++ circleguard/settings.py
@@ -18,10 +18,10 @@ def reset_defaults():
     SETTINGS.setValue("local_replay_dir", "./examples/replays/")
 
     # string settings
-    SETTINGS.setValue("message_loading_replays", "[{ts:%H}:{ts:%M}:{ts:%S}] Loading {num_replays} Replays")
-    SETTINGS.setValue("message_starting_comparing", "[{ts:%H}:{ts:%M}:{ts:%S}] Comparing Replays")
-    SETTINGS.setValue("message_finished_comparing", "[{ts:%H}:{ts:%M}:{ts:%S}] Done")
-    SETTINGS.setValue("message_cheater_found", "[{ts:%H}:{ts:%M}:{ts:%S}] {similarity} similarity. {replay1_name} vs {replay2_name}, {later_name} set later")
+    SETTINGS.setValue("message_loading_replays", "[{ts:%X}] Loading {num_replays} Replays")
+    SETTINGS.setValue("message_starting_comparing", "[{ts:%X}] Comparing Replays")
+    SETTINGS.setValue("message_finished_comparing", "[{ts:%X}] Done")
+    SETTINGS.setValue("message_cheater_found", "[{ts:%X}] {similarity:.1f} similarity. {replay1_name} vs {replay2_name}, {later_name} set later")
 
     SETTINGS.sync()
 
