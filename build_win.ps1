e00ef5176c090db0b26028f483f2807540b8a280
Liam DeVoe <orionldevoe@gmail.com>
2019-10-10 10:56:07 -0400

    change windows requirements location

--- build_win.ps1
+++ build_win.ps1
@@ -4,7 +4,7 @@ Write-Output "activating venv"
 ./build-env/Scripts/activate.ps1
 # install pip requirements
 Write-Output "installing pip requirements"
-pip install -r requirements.txt -r requirements_build.txt
+pip install -r requirements.txt -r requirements_build_win.txt
 # run pyinstaller for x64 & x86
 Write-Output "run x64 build"
 pyinstaller gui_win_x64.spec --noconfirm
