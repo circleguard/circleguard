af40b2718680fa20c583bcd7c52d4bd613e0254a
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-10-18 16:52:00 +0200

    increase clock resolution from 1ms to 1μs
    1000x the resolution.
    Nice

--- circleguard/clock.py
+++ circleguard/clock.py
@@ -7,25 +7,27 @@ class Timer:
         self.current_speed = 1
         self.paused = False
         self.paused_at_run_time = None
-        self.last_run_time = time.time_ns()
+        self.last_run_time = time.perf_counter()
 
     def get_time(self):
+        current_run_time = time.perf_counter()
         if not self.paused:
-            current_run_time = time.time_ns()
             time_took = current_run_time-self.last_run_time
-            self.time_counter += time_took/1000000*self.current_speed
-            self.last_run_time = time.time_ns()
+            self.time_counter += (time_took*1000)*self.current_speed
+            self.last_run_time = current_run_time
         return self.time_counter
 
     def pause(self):
+        current_run_time = time.perf_counter()
         if not self.paused:
             self.paused = True
-            self.paused_at_run_time = time.time_ns()
+            self.paused_at_run_time = current_run_time
         return self.get_time()
 
     def resume(self):
+        current_run_time = time.perf_counter()
         if self.paused:
-            self.last_run_time += (time.time_ns()-self.paused_at_run_time)
+            self.last_run_time += (current_run_time-self.paused_at_run_time)
             self.paused_at_run_time = None
             self.paused = False
         return self.get_time()
