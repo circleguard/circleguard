6810ebcd10201e49df80f829758a0f1293209cfc
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-10-20 00:49:57 +0200

    match output with bash script

--- build_win.ps1
+++ build_win.ps1
@@ -1,17 +1,21 @@
 Write-Output "creating venv"
 python -m venv build-env --clear
+
 Write-Output "activating venv"
 ./build-env/Scripts/activate.ps1
-# install pip requirements
-Write-Output "installing pip requirements"
-pip install -r requirements.txt -r requirements_build_win.txt
-# run pyinstaller for x64 & x86
-Write-Output "run x64 build"
+
+echo "installing pip requirements"
+pip install -r requirements.txt
+echo "installing build requirements"
+pip install -r requirements_build_win.txt
+
+Write-Output "building x64"
 pyinstaller gui_win_x64.spec --noconfirm
-Write-Output "run x86 build"
+Write-Output "building x86"
 pyinstaller gui_win_x86.spec --noconfirm
-# leave and delete venv
+
 Write-Output "deactivating venv"
 deactivate
+
 Write-Output "deleting venv"
 Remove-Item ./build-env/ -Recurse -Force
