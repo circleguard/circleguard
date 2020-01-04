e49e07a13e1bc37679e0dba12bab80e3b0e164c6
Liam DeVoe <orionldevoe@gmail.com>
2020-01-04 18:08:47 -0500

    better relax messages

--- circleguard/settings.py
+++ circleguard/settings.py
@@ -147,8 +147,8 @@ DEFAULTS = {
         # it is possible though extremely unusual for the replays to have different map ids. This is good enough
         "message_steal_found": "[{ts:%X}] {sim:.1f} similarity. {replay1.username} vs {replay2.username} on map {replay1.map_id}, {r.later_replay.username} set later. Extremely similar replays; look at the visualization to investigate further.",
         "message_steal_found_display": "[{ts:%X}] {similarity:.1f} similarity. {r1.username} vs {r2.username} on map {r1.map_id}. Replays likely not stolen.",
-        "message_relax_found": "ya you got a relax'd play with ur {ur}",
-        "message_relax_found_display": "possibly cheated play with ur {ur}"
+        "message_relax_found": "[{ts:%X}] {ur:.2f} ur. {replay.username} +{replay.mods.short_name} on map {replay.map_id}", # TODO make ModCombination short_name and long_name be @property's so we can reference them here and still keep them as function's in circlecore.
+        "message_relax_found_display": "[{ts:%X}] {ur:.2f} ur. {replay.username} +{replay.mods.short_name} on map {replay.map_id} "
     },
     "Strings": {
         "string_result_text": "[{ts:%x %H:%M}] {similarity:.1f} similarity. {r.later_replay.username} (set later) vs {r.earlier_replay.username} on map {r1.map_id}",
