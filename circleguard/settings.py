e3a47095a0751a3c73f0eebdc33e3f15abb15caa
Liam DeVoe <orionldevoe@gmail.com>
2020-01-14 21:39:06 -0500

    thinner border for required_style

--- circleguard/settings.py
+++ circleguard/settings.py
@@ -185,8 +185,8 @@ DEFAULTS = {
         "dark_theme": False,
         "visualizer_info": True,
         "visualizer_bg": False,
-        "required_style": "QLineEdit { border: 2px solid red }\n"
-                          "WidgetCombiner { border: 2px solid red }"
+        "required_style": "QLineEdit { border: 1px solid red }\n"
+                          "WidgetCombiner { border: 1px solid red }"
     },
     "Experimental": {
         "rainbow_accent": False
