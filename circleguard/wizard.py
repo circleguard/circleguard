ff1774ef8f04cd4b2f2b185334a15813adb97ab5
Liam DeVoe <orionldevoe@gmail.com>
2020-03-15 11:40:23 -0400

    reword beatmap urls tutorial

--- circleguard/wizard.py
+++ circleguard/wizard.py
@@ -127,7 +127,7 @@ class TutorialPageId(WizardPage):
                 "To find a User id, go to their profile page and copy the numbers that appear in the url. "
                 "For instance, cookiezi (<a href=\"https://osu.ppy.sh/users/124493\">https://osu.ppy.sh/users/124493</a>) "
                 "has a User id of 124493.</p>"
-                "<p>Map ids are slightly trickier - the url contains two ids. The first is the Beatmapset id, and "
+                "<p>Map urls contain two ids. The first is the Beatmapset id, and "
                 "the second is the Beatmap id. You want to use the latter (the Beatmap id)"
                 "For instance, <a href=\"https://osu.ppy.sh/beatmapsets/39804#osu/129891\">"
                 "https://osu.ppy.sh/beatmapsets/39804#osu/129891</a> has a Beatmapset id of 39804 and a Beatmap id of "
