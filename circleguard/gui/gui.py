220f14ffc21fbc4d3fb0592aa162c6cfe59fefa7
Liam DeVoe <orionldevoe@gmail.com>
2020-06-30 00:04:03 -0400

    replace bug report with discord link (not like anyone reports bugs on github lol)

--- circleguard/gui/gui.py
+++ circleguard/gui/gui.py
@@ -191,7 +191,7 @@ class SettingsTab(QFrame):
         # https://groups.google.com/forum/#!topic/qtcontribs/VDOQFUj-eIA
         self.info.setText(f"circleguard v{__version__}&nbsp;&nbsp;|&nbsp;&nbsp;"
                           f"circlecore v{cg_version}&nbsp;&nbsp;|&nbsp;&nbsp;"
-                          f"<a href=\"https://github.com/circleguard/circleguard/issues\">Bug Report</a>")
+                          f"<a href=\"https://discord.gg/wj35ehD\">Discord</a>")
         self.info.setTextFormat(Qt.RichText)
         self.info.setTextInteractionFlags(Qt.TextBrowserInteraction)
         self.info.setOpenExternalLinks(True)
