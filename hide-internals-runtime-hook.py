e3a8cf144620398fbfb3e089d6c29b1f0937699c
Liam DeVoe <orionldevoe@gmail.com>
2020-10-19 15:53:26 -0400

    attempt to move files in dist folder to a lib folder

---
+++ hide-internals-runtime-hook.py
@@ -0,0 +1,4 @@
+import sys
+import os
+# http://unafaltadecomprension.blogspot.com/2014/07/pyinstaller-separating-executable-from.html
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "internals"))
