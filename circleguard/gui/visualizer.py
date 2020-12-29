ba9f9dfb1a9f3e4e352b51269a6dcf2aad3c743d
Liam DeVoe <orionldevoe@gmail.com>
2020-12-29 00:36:56 -0500

    have visualizer inherit snaps behavior of gui setting

--- circleguard/gui/visualizer.py
+++ circleguard/gui/visualizer.py
@@ -10,6 +10,9 @@ def get_visualizer():
         def __init__(self, beatmap_info, replays=[], events=[], library=None):
             speeds = get_setting("speed_options")
             start_speed = get_setting("default_speed")
-            super().__init__(beatmap_info, replays, events, library, speeds, start_speed)
+            snaps_args = {"only_on_hitobjs": get_setting("ignore_snaps_off_hitobjs")}
+
+            super().__init__(beatmap_info, replays, events, library, speeds, start_speed, snaps_args=snaps_args)
             self.setWindowIcon(QIcon(resource_path("logo/logo.ico")))
+
     return CGVisualizer
