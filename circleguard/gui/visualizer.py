41dc1b99fdce5fd450a229e9f54d23d8ad518b40
Liam DeVoe <orionldevoe@gmail.com>
2021-04-03 13:40:11 -0400

    pass max_angle and min_distance to visualizer

--- circleguard/gui/visualizer.py
+++ circleguard/gui/visualizer.py
@@ -10,7 +10,11 @@ def get_visualizer():
         def __init__(self, beatmap_info, replays=[], events=[], library=None):
             speeds = get_setting("speed_options")
             start_speed = get_setting("default_speed")
-            snaps_args = {"only_on_hitobjs": get_setting("ignore_snaps_off_hitobjs")}
+            snaps_args = {
+                "max_angle": get_setting("correction_max_angle"),
+                "min_distance": get_setting("correction_min_distance"),
+                "only_on_hitobjs": get_setting("ignore_snaps_off_hitobjs")
+            }
 
             super().__init__(beatmap_info, replays, events, library, speeds, start_speed, snaps_args=snaps_args)
             self.setWindowIcon(QIcon(resource_path("logo/logo.ico")))
