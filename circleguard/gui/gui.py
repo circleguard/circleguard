afa809e7827586bc1f3768b5939f8c21cb14f697
Liam DeVoe <orionldevoe@gmail.com>
2022-01-05 18:35:59 -0500

    remove unused variable

--- circleguard/gui/gui.py
+++ circleguard/gui/gui.py
@@ -255,7 +255,7 @@ class AnalysisSelection(QFrame):
             # TODO reuse global library here
             self.visualizer = CGVisualizer(beatmap_info, loadables)
             self.visualizer.show()
-        except InvalidKeyException as e:
+        except InvalidKeyException:
             message = ("Please enter a valid api key in the settings before "
                 "visualizing replays.")
             self.show_exception_signal.emit(message)
